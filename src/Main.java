69f495a1fb63322306e0873942c657e90e1a18ed
CLacaile <clementlacaile@gmail.com>
2018-12-04 20:26:03 +0100

    added test for fournisseurs + factures

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
-import controller.DAO;
-import controller.DAOClient;
-import controller.TPController;
-import model.Client;
-import model.TPModel;
+import controller.*;
+import model.*;
 import view.TPView;
 
 public class Main {
@@ -12,15 +9,40 @@ public class Main {
         TPView theView = new TPView();
         TPController theController = new TPController(theModel, theView);
 
+        System.out.println("************* Liste des clients *************");
         DAO<Client> daoclient = new DAOClient();
-
         for(int i=1; i<3; i++) {
             System.out.println("---------- Client #"+i+" ----------");
             System.out.println("Nom: "+ daoclient.find(i).getNomC());
             System.out.println("Prenom: "+ daoclient.find(i).getPrenomC());
-            System.out.println("Nom: "+ daoclient.find(i).getNomC());
+            System.out.println("Ville: "+ daoclient.find(i).getVilleC());
             System.out.println("Adresse: "+ daoclient.find(i).getAdresseC());
             System.out.println("Reduc: "+ daoclient.find(i).getReductionC());
         }
+
+        System.out.println("************* Liste des fournisseurs *************");
+        DAO<Fournisseur> daofournisseur = new DAOFournisseur();
+        for(int i=1; i<3; i++) {
+            System.out.println("---------- Fournisseur #"+i+" ----------");
+            System.out.println("Nom: "+ daofournisseur.find(i).getNomF());
+            System.out.println("Prenom: "+ daofournisseur.find(i).getPrenomF());
+            System.out.println("Ville: "+ daofournisseur.find(i).getVilleF());
+            System.out.println("Adresse: "+ daofournisseur.find(i).getAdresseF());
+        }
+
+        System.out.println("************* Liste des factures *************");
+        DAO<Facture> daofacture = new DAOFacture();
+        for(int i=1; i<=3; i++) {
+            Client c = daofacture.find(i).getClientFact();
+            System.out.println("---------- Facture #"+i+" ----------");
+            System.out.println("N°: "+ daofacture.find(i).getIdFact());
+            System.out.println("Client: "+ c.getPrenomC() + " " + c.getNomC());
+            for(int j=0; j<daofacture.find(i).getProduitsFact().size(); j++) {
+                Produit p = daofacture.find(i).getProduitsFact().get(j);
+                System.out.println("Produit #"+p.getIdP()+":\t"+p.getNomP()+"\t"+p.getPrixP()+"\t"+daofacture.find(i).getQteProdFact().get(j));
+            }
+            System.out.println("Montant total: "+daofacture.find(i).getMontantFact());
+        }
+
     }
 }
