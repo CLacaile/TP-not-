8d43aabcbc0181dad0f6d092e742d7a2ea678ac6
CLacaile <clementlacaile@gmail.com>
2018-12-04 19:22:07 +0100

    added attributes and construct

--- src/controller/TPController.java
+++ src/controller/TPController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import model.TPModel;
+import view.TPView;
+
 public class TPController {
+    private TPModel model;
+    private TPView view;
+
+    public TPController(TPModel m, TPView v) {
+        this.model = m;
+        this.view = v;
+    }
 }
