ac8f013878b5232cab85225b501701bbb7b7ffde
CLacaile <clementlacaile@gmail.com>
2018-12-05 23:03:51 +0100

    added EditionFactureView

--- src/view/FactureView/EditionFactureView.java
+++ src/view/FactureView/EditionFactureView.java
@@ -1,6 +1,7 @@
 package view.FactureView;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class EditionFactureView extends JPanel {
     private JTable clientFactureTable;
@@ -15,4 +16,37 @@ public class EditionFactureView extends JPanel {
     private JLabel tauxTVALabel;
     private JLabel montantTotalTitleLabel;
     private JLabel montantTotalLabel;
+
+    private FactureGroupBox factBox;
+    private ClientGroupBox clientBox;
+
+    public EditionFactureView() {
+        // Layout
+        setLayout(new GridBagLayout());
+        // Contraintes
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.insets = new Insets(2,2,2,2);
+        gc.weightx = 5;
+        gc.weighty = 14;
+
+        gc.anchor = GridBagConstraints.NORTH;
+        // Client Box
+        clientBox = new ClientGroupBox();
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.gridwidth = 5;
+        gc.gridheight = 8;
+        gc.fill = GridBagConstraints.HORIZONTAL;
+        this.add(clientBox, gc);
+
+        gc.anchor = GridBagConstraints.SOUTH;
+        // Edit Facture Box
+        factBox = new FactureGroupBox();
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.gridwidth = 1;
+        gc.gridheight = 1;
+        gc.fill = GridBagConstraints.HORIZONTAL;
+        this.add(factBox, gc);
+    }
 }
--- src/view/TPView.java
+++ src/view/TPView.java
@@ -1,9 +1,13 @@
 package view;
 
+import view.FactureView.EditionFactureView;
+import view.FactureView.FactureView;
+
 import javax.swing.*;
 
 public class TPView extends JFrame {
     private ClientView clientPanel;
+    private EditionFactureView editFactPanel;
 
     public TPView() {
         JTabbedPane tabbedPane = new JTabbedPane();
@@ -12,6 +16,11 @@ public class TPView extends JFrame {
         this.clientPanel = new ClientView();
         tabbedPane.addTab("Clients", null, clientPanel, "Voir et gérer les clients");
 
+        // Onglets "Facture"
+        this.editFactPanel = new EditionFactureView();
+        tabbedPane.addTab("Editer une facture", null, editFactPanel, "Créer une nouvelle facture");
+
+        // Général
         this.setTitle("Outil de gestion pour fleuriste");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(800, 600);
