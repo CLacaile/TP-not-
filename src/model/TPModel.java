13bdf4ad3e7f37b66289da8e2393dde42a5c2832
CLacaile <clementlacaile@gmail.com>
2018-12-09 19:42:55 +0100

    added produits attribute

--- src/model/TPModel.java
+++ src/model/TPModel.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class TPModel {
     private ArrayList<Client> clients = new ArrayList<Client>();
+    private ArrayList<Produit> produits = new ArrayList<Produit>();
 
     public ArrayList<Client> getClients() {
         return clients;
@@ -12,4 +13,14 @@ public class TPModel {
     public void setClients(ArrayList<Client> clients) {
         this.clients = clients;
     }
+
+    public ArrayList<Produit> getProduits() {
+        return produits;
+    }
+
+    public void setProduits(ArrayList<Produit> produits) {
+        this.produits = produits;
+    }
+
+
 }
